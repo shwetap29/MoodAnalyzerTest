e96a7ade16671af689a4c7ea58d77f82b1acf9df
shwetap29 <shwetaprasad48@gmail.com>
2021-06-03 22:52:12 +0530

    ADDED:Given Empty Mood should Throw MoodAnalysisException

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -7,13 +7,46 @@ import static org.junit.Assert.assertSame;
 public class MoodAnalyzerTest {
     //Test case for Sad mood
     @Test
-    public void given_NullMood_Should_Throw_MoodAnalysisException() {
+    public void given_SadMood_Should_Return_Sad() {
         MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain(null);
         String mood;
         try {
             mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("SAD", mood);
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.Exception_Type.NULL,e.type);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void given_NullMood_Should_Throw_MoodAnalysisException() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain(null);
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertSame(MoodAnalysisException.Exception_Type.NULL, e.type);
+        }
+    }
+
+    @Test
+    public void given_EmptyMood_Should_Throw_MoodAnalysisException() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("");
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertSame(MoodAnalysisException.Exception_Type.EMPTY, e.type);
         }
     }
 }
