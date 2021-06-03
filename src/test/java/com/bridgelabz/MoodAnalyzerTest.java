c2f5d6ae2abdaa167fe23ffba4d7aac233cf3558
shwetap29 <shwetaprasad48@gmail.com>
2021-06-03 22:12:09 +0530

    ADDED:Functionality To Throw Custom Exceptions

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -7,23 +7,26 @@ public class MoodAnalyzerTest {
     //Test case for Sad mood
     @Test
     public void given_SadMood_Should_Return_SAD() {
-        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("I am In Sad Mood");
-        String mood = moodAnalyzer.analyseMood();
-        assertSame("SAD", mood);
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am In Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
     //Constructor in message should return Happy
 
     @Test
     public void given_AnyMood_Should_Return_HAPPY() {
         MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
-    }
-
-    @Test
-    public void given_NULLMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain(null);
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
